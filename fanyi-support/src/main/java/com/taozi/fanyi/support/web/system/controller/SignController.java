a9e6223ab7868075f479ce94dbac9225d95640ee
yangguangpiaosa <yangguangpiaosa@163.com>
2016-07-28 18:11:07 +0800

    update maven settings.

--- fanyi-support/src/main/java/com/taozi/fanyi/support/web/system/controller/SignController.java
+++ fanyi-support/src/main/java/com/taozi/fanyi/support/web/system/controller/SignController.java
@@ -21,8 +21,8 @@ public class SignController extends Controller {
 		User user = User.dao.findFirst("select * from user where email = ? and user_pass = ?", email, Encryptor.base64AfterMd5(password));
 		if(count == 1 && null != user) {
 			log.info(user.getUserName() + " - " + user.getEmail());
-			setSessionAttr(user.getId().toString(), user);
-			log.info(getSessionAttr(user.getId().toString()).toString());
+			setSessionAttr("USER", user);
+			log.info(getSessionAttr("USER").toString());
 			//forward to admin action
 			redirect("/home");
 		} else {
